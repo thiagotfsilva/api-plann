dc555166abfc3a2d0707648e2a94803661f66da6
thiagotfsilva <thiagophsilva@hotmail.com>
2025-08-07 16:26:05 -0300

    configuração prettier e eslint

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+// eslint.config.js
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+
+module.exports = [
+  {
+    ignores: ['.eslintrc.js', 'dist/**/*'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: 'tsconfig.json',
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+      },
+      env: {
+        node: true,
+        jest: true,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/explicit-module-boundary-types': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/prefer-nullish-coalescing': 'error',
+      '@typescript-eslint/prefer-optional-chain': 'error',
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      'prettier',
+    ],
+  },
+];
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,5 @@
 import Fastify from 'fastify';
 
-const app = Fastify({
-    logger: true
-})
+const app = Fastify({ logger: true });
 
-app.listen({port: 3000}, () => console.log('App is runnings'))
-
+app.listen({ port: 3000 }, () => console.log('App is runnings'));
